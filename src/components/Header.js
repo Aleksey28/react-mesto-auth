39515140d535a63440ed4fcb803949b73c756fd7
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-01-19 22:47:03 +0400

    feat: changed styles for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 import { Route } from "react-router";
 import cn from "classnames";
 
-export default function Header({ logo }) {
+export default function Header({ logo, isLogin = true }) {
 
   const [menuIsOpen, setMenuIsOpen] = useState(false);
 
@@ -12,26 +12,14 @@ export default function Header({ logo }) {
   };
 
   return (
-    <header className="header">
-      <Route exact path="/">
-        <div className={cn("header__menu header__menu_vertical", { header__menu_hidden: !menuIsOpen })}>
-          <p className="header__email">Email</p>
-          <NavLink to="/sign-up" className="header__nlink">Выйти</NavLink>
-        </div>
-      </Route>
-      <div className="header__main">
-        <a href="#0">
-          <img src={logo} alt="Логотип страницы Место." className="header__logo"/>
-        </a>
+    <header className={cn("header", {header_compact: isLogin})}>
+      <a href="#0">
+        <img src={logo} alt="Логотип страницы Место." className="header__logo"/>
+      </a>
+      <div className={cn("header__navbar", { header__navbar_vertical: isLogin, header__navbar_hidden: isLogin && !menuIsOpen })}>
         <Route exact path="/">
-          <div className="header__menu">
-            <p className="header__email">Email</p>
-            <NavLink to="/sign-up" className="header__nlink">Выйти</NavLink>
-          </div>
-          <button className={cn(
-            "header__btn",
-            { header__btn_type_close: menuIsOpen, header__btn_type_open: !menuIsOpen },
-          )} onClick={handleClickOnSwitch}/>
+          <p className="header__email">Email</p>
+          <NavLink to="/sign-up" className="header__nlink header__nlink_type_exit">Выйти</NavLink>
         </Route>
         <Route path="/sign-in">
           <NavLink to="/sign-up" className="header__nlink">Регистрация</NavLink>
@@ -40,6 +28,10 @@ export default function Header({ logo }) {
           <NavLink to="/sign-in" className="header__nlink">Войти</NavLink>
         </Route>
       </div>
+      {isLogin && <button className={cn(
+        "header__btn",
+        { header__btn_type_close: menuIsOpen, header__btn_type_open: !menuIsOpen },
+      )} onClick={handleClickOnSwitch}/>}
     </header>
   );
 }
